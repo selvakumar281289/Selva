e74bab4bf4f1365e7d434181be7ec9c976e890c2
selvakumar281289 <selvakumaranbalagan281289@gmail.com>
2019-11-08 15:36:02 +0530

    I have done my stories

---
+++ src/org/demo/sample.java
@@ -0,0 +1,11 @@
+package org.demo;
+
+public class sample {
+	public void emID () {
+		System.out.println("This is Employee ID");
+	}
+public void employeeName() {
+	System.out.println("This is Employee name");
+	
+}
+}
